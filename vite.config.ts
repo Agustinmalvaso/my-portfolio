5c24d1d4a53d244c0f2f2a92434b6e9a039e9b33
Agustin Malvaso <agustin.malvaso@gmail.com>
2024-08-08 00:29:22 -0300

    Installed tresjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { templateCompilerOptions } from '@tresjs/core'
 
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -11,7 +12,9 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  plugins: [vue()],
+  plugins: [vue({
+    ...templateCompilerOptions
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
